aa2868152acf8e29ff4bf30a03b902a79f1114f2
Nicholas Kyriakides <nicholaswmin@Nicholass-MacBook-Air.local>
2024-07-22 19:00:18 +0300

    style: fix a lint error

--- test/unit/reset.test.js
+++ test/unit/reset.test.js
@@ -2,7 +2,7 @@ import test from 'node:test'
 import { Recordable } from '../../index.js'
 
 await test('#reset()', async t => {
-  let recordable, histogram = null
+  let recordable = null
 
   t.beforeEach(() => {
     recordable = new Recordable()
